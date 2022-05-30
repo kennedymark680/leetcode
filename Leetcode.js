f03c51187ca7611c4506d2c5ccce863332443ae0
Mark Kennedy <kennedymark@hotmail.com>
2022-05-30 09:32:48 -0400

    completed 1365

--- Leetcode.js
+++ Leetcode.js
@@ -1,1 +1,15 @@
-console.log('test')
+// 1365
+
+let nums = [8, 1, 2, 2, 3]
+
+let count = []
+
+for (let i = 0; i < nums.length; i++) {
+  let acc = 0
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[i] > nums[j]) acc++
+  }
+  count.push(acc)
+}
+
+console.log(count)
