b5851ec7d6daeea1010ce43c2868dea671829469
Mark Kennedy <kennedymark@hotmail.com>
2022-05-31 09:23:33 -0400

    completed 1748

--- Leetcode.js
+++ Leetcode.js
@@ -16,16 +16,34 @@
 
 // 1313
 
-let nums = [1, 2, 3, 4]
+// let nums = [1, 2, 3, 4]
+
+// let arr = []
+
+// for (let i = 0; i < nums.length; i += 2) {
+//   let times = nums[i]
+//   while (times > 0) {
+//     arr.push(nums[i + 1])
+//     times -= 1
+//   }
+// }
+
+// console.log(arr)
+
+// 1748
+
+let nums = [1, 2, 3, 2]
 
 let arr = []
 
-for (let i = 0; i < nums.length; i += 2) {
-  let times = nums[i]
-  while (times > 0) {
-    arr.push(nums[i + 1])
-    times -= 1
+for (let i = 0; i < nums.length; i++) {
+  let count = 0
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[i] === nums[j]) count++
   }
+  if (count === 1) arr.push(nums[i])
 }
 
-console.log(arr)
+let answer = arr.reduce((t, a) => t + a)
+
+console.log(answer)
