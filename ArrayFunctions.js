83d95796052207a6fff0c513ac2cb46a02502cda
Mark Kennedy <kennedymark@hotmail.com>
2022-06-02 09:22:38 -0400

    practice array functions - filter

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -2,5 +2,5 @@ let numbers = [1, 2, 3, 4, 5]
 let letters = ['a', 'b', 'c', 'd', 'e']
 let words = ['one', 'two', 'three']
 
-const mapped = numbers.map((e) => e + 10)
-console.log(mapped)
+const test = numbers.filter((e) => e === 2)
+console.log(test)
