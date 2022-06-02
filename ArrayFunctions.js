87ffb7204b8171df34f8e9e6135d40e9e5728560
Mark Kennedy <kennedymark@hotmail.com>
2022-06-02 09:27:25 -0400

    practice array functions - every

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -2,5 +2,5 @@ let numbers = [1, 2, 3, 4, 5]
 let letters = ['a', 'b', 'c', 'd', 'e']
 let words = ['one', 'two', 'three']
 
-const test = numbers.concat(letters)
+const test = numbers.every((e) => e === 3)
 console.log(test)
