3fd1796bc848afa8ebfc416d2365c34bfaa8ff12
Mark Kennedy <kennedymark@hotmail.com>
2022-06-02 09:25:41 -0400

    practice array functions - forEach

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -2,5 +2,5 @@ let numbers = [1, 2, 3, 4, 5]
 let letters = ['a', 'b', 'c', 'd', 'e']
 let words = ['one', 'two', 'three']
 
-const test = numbers.sort((a, b) => (a > b ? -1 : 1))
+const test = numbers.forEach((e) => console.log(e))
 console.log(test)
