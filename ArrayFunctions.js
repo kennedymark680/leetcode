d277c6824d01a88df1df0f8475849723c61b81b3
Mark Kennedy <kennedymark@hotmail.com>
2022-06-02 09:26:25 -0400

    practice array functions - concat

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -2,5 +2,5 @@ let numbers = [1, 2, 3, 4, 5]
 let letters = ['a', 'b', 'c', 'd', 'e']
 let words = ['one', 'two', 'three']
 
-const test = numbers.forEach((e) => console.log(e))
+const test = numbers.concat(letters)
 console.log(test)
