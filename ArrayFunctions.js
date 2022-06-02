7093b3ec1eef49adad7db36fbf2f384535d0b1d5
Mark Kennedy <kennedymark@hotmail.com>
2022-06-02 09:23:48 -0400

    practice array functions - sort

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -2,5 +2,5 @@ let numbers = [1, 2, 3, 4, 5]
 let letters = ['a', 'b', 'c', 'd', 'e']
 let words = ['one', 'two', 'three']
 
-const test = numbers.filter((e) => e === 2)
+const test = numbers.sort((a, b) => (a > b ? -1 : 1))
 console.log(test)
