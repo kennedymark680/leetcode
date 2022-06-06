dcf6e12d3696392c4374075a9b50d8b9f7395c86
Mark Kennedy <kennedymark@hotmail.com>
2022-06-06 10:20:18 -0400

    leetcode - 771 another answer

--- StringLeetcode.js
+++ StringLeetcode.js
@@ -1,14 +1,20 @@
 // 771
 
-let jewels = 'aA'
-let stones = 'aAAbbbb'
+// let jewels = 'aA'
+// let stones = 'aAAbbbb'
 
-let count = 0
+// let count = 0
 
-for (let i = 0; i < jewels.length; i++) {
-  for (let j = 0; j < stones.length; j++) {
-    if (jewels[i] === stones[j]) count++
-  }
-}
+// for (let i = 0; i < jewels.length; i++) {
+//   for (let j = 0; j < stones.length; j++) {
+//     if (jewels[i] === stones[j]) count++
+//   }
+// }
 
-console.log(count, 'count')
+// console.log(count, 'count')
+
+// for (let x of stones) {
+//   if (jewels.includes(x)) count++
+// }
+
+// console.log(count, 'count')
