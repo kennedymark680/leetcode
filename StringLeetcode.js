194633c2ddbbd0709e44045729037624036d6ae6
Mark Kennedy <kennedymark@hotmail.com>
2022-06-06 19:14:53 -0400

    completed 1678

--- StringLeetcode.js
+++ StringLeetcode.js
@@ -19,19 +19,29 @@
 
 // console.log(count, 'count')
 
-let count = 1
-for (let i = 0; i < 100; i++) {
-  if (count % 3 === 0 && count % 5 === 0) {
-    console.log('FizzBuzz')
-    count++
-  } else if (count % 3 === 0) {
-    console.log('Fizz')
-    count++
-  } else if (count % 5 === 0) {
-    console.log('Buzz')
-    count++
-  } else {
-    console.log(count)
-    count++
-  }
-}
+// FIZZBUZZ
+
+// let count = 1
+// for (let i = 0; i < 100; i++) {
+//   if (count % 3 === 0 && count % 5 === 0) {
+//     console.log('FizzBuzz')
+//     count++
+//   } else if (count % 3 === 0) {
+//     console.log('Fizz')
+//     count++
+//   } else if (count % 5 === 0) {
+//     console.log('Buzz')
+//     count++
+//   } else {
+//     console.log(count)
+//     count++
+//   }
+// }
+
+// 1678
+
+let command = 'G()()()(al)'
+
+command.replace('()', 'o')
+
+console.log(command.replaceAll('()', 'o').replaceAll('(al)', 'al'), 'command')
