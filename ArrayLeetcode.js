022583c5e087b4e3e9e759c77cc2ac1e1ddcba93
Mark Kennedy <kennedymark@hotmail.com>
2022-06-17 22:41:26 -0400

    167

--- ArrayLeetcode.js
+++ ArrayLeetcode.js
@@ -252,13 +252,13 @@ let end = numbers.length - 1
 
 numbers.indexOf()
 
-// for (let i = 0; i < numbers.length; i++) {
-//   for (let j = i + 1; j < numbers.length; j++) {
-//     if (numbers[i] + numbers[j] === target) {
-//       sum.push(i + 1)
-//       sum.push(j + 1)
-//     }
-//   }
-// }
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = i + 1; j < numbers.length; j++) {
+    if (numbers[i] + numbers[j] === target) {
+      sum.push(i + 1)
+      sum.push(j + 1)
+    }
+  }
+}
 
 console.log(sum)
